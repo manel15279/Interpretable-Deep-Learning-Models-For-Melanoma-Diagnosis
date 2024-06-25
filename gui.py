7dcc477bd28341f5479e945f117f05818bf34143
manel15279 <manelouchar@users.noreply.github.com>
2024-06-25 11:23:43 +0100

    models

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import gradio as gr
 
 class App:
+    def __init__(self) -> None:
+        self.create_interface()
+        
     def create_interface(self):
         with gr.Blocks() as demo:
 
@@ -8,8 +11,13 @@ class App:
             with gr.Tab("Heatmaps"):
                 gr.Markdown("""Upload the lesion image and visualize""")
                 with gr.Column():
-                    
-
+                    with gr.Row():
+                        with gr.Column():
+                            model = [gr.Dropdown(["ResNet50", "DenseNet201", 'MobileNet', 'Xception', 'InceptionV3', 'VGG19', 'VGG16'], multiselect=False, label="Models", info="Select a model : ")]
+                            layer = [gr.Dropdown([(f"{layer.name}", i) for i, layer in enumerate(model.layers)], multiselect=False, label="Layers", info="Select a layer of the model : ")]
+                            htm = [gr.Checkbox(['GradCAM', 'GradCAM++', 'ScoreCAM'], label='Visualization Method', info='Select one or more methods to visualize the heatmaps :')]
+                        with gr.Column():
+                            image = gr.Image(label="Upload an Image", type="filepath")
                
 
             
